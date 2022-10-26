d7030d22f4d37d6386256d29a70dacf286607f29
7c00 <i@7c00.cc>
2022-10-26 13:37:08 +0800

    Example spectrum

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:scidart/numdart.dart';
 import 'package:sound_recording/sound_recording.dart';
+import 'package:scidart/scidart.dart';
 
 void main() {
   runApp(const App());
@@ -98,21 +100,41 @@ class WaveformPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    final slice = size.width / (data.length - 1);
-    var x = 0.0;
-    final points = data.map((i) {
-      final y = (0.5 + i / 16384) * size.height;
-      final offset = Offset(x, y);
-      x += slice;
-      return offset;
-    });
-    canvas.drawPoints(
-      PointMode.lines,
-      points.toList(),
-      Paint()
-        ..strokeWidth = 1
-        ..color = Colors.black54,
-    );
+    {
+      final spectrum = rfft(Array(data.map((i) => i.toDouble()).toList()));
+      final items = spectrum.sublist(0, spectrum.length ~/ 2);
+      final slice = size.width / (items.length - 1);
+      var x = 0.0;
+      for (final i in items) {
+        items.indexOf(i);
+        final abs = complexAbs(i);
+        final y = (abs / 262144) * size.height;
+        final color =
+            HSLColor.fromAHSL(1, items.indexOf(i) * 360 / items.length, 1, 0.5);
+        canvas.drawRect(
+          Rect.fromLTWH(x, size.height - y, slice, y),
+          Paint()..color = color.toColor(),
+        );
+        x += slice;
+      }
+    }
+    {
+      final slice = size.width / (data.length - 1);
+      var x = 0.0;
+      final wavePoints = data.map((i) {
+        final y = (0.5 + i / 32768) * size.height;
+        final offset = Offset(x, y);
+        x += slice;
+        return offset;
+      });
+      canvas.drawPoints(
+        PointMode.lines,
+        wavePoints.toList(),
+        Paint()
+          ..strokeWidth = 1
+          ..color = Colors.grey,
+      );
+    }
   }
 
   @override
